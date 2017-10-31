42d143c9b7b17d0917a7d5c336d2426c34e70e30
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-10-31 18:26:05 +0300

    project TLIB.00 added permissions

--- Adv02/AdvancedAndroid_Emojify.TLIB.00/app/src/main/java/com/example/android/emojify/MainActivity.java
+++ Adv02/AdvancedAndroid_Emojify.TLIB.00/app/src/main/java/com/example/android/emojify/MainActivity.java
@@ -20,6 +20,7 @@ package com.example.android.emojify;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -38,6 +39,7 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -72,6 +74,8 @@ public class MainActivity extends AppCompatActivity {
         mSaveFab = (FloatingActionButton) findViewById(R.id.save_button);
         mClearFab = (FloatingActionButton) findViewById(R.id.clear_button);
         mTitleTextView = (TextView) findViewById(R.id.title_text_view);
+
+
     }
 
     /**
@@ -138,13 +142,21 @@ public class MainActivity extends AppCompatActivity {
                 // Get the path of the temporary file
                 mTempPhotoPath = photoFile.getAbsolutePath();
 
+
                 // Get the content URI for the image file
                 Uri photoURI = FileProvider.getUriForFile(this,
                         FILE_PROVIDER_AUTHORITY,
                         photoFile);
-
+                revokeUriPermission(photoURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 // Add the URI so the camera can store the image
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+
+                List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                for (ResolveInfo resolveInfo : resInfoList) {
+                    String packageName = resolveInfo.activityInfo.packageName;
+                    grantUriPermission(packageName, photoURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                }
 
                 // Launch the camera activity
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
