d8b055d6f0524098dfe99f6380678689055da2fb
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-11-03 10:38:16 +0300

    Squawker project update TFCM.04 finished

--- Adv03/Squawker.TCFM.04/app/src/main/java/android/example/com/squawker/MainActivity.java
+++ Adv03/Squawker.TCFM.04/app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -113,12 +113,12 @@ public class MainActivity extends AppCompatActivity implements
         
         // Gets the extra data from the intent that started the activity. For *notification*
         // messages, this will contain key value pairs stored in the *data* section of the message.
-        Bundle extras = getIntent().getExtras();
-        // Checks if the extras exist and if the key "test" from our FCM message is in the intent
-        if (extras != null && extras.containsKey("test")) {
-            // If the key is there, print out the value of "test"
-            Log.d(LOG_TAG, "Contains: " + extras.getString("test"));
-        }
+//        Bundle extras = getIntent().getExtras();
+//        // Checks if the extras exist and if the key "test" from our FCM message is in the intent
+//        if (extras != null && extras.containsKey("test")) {
+//            // If the key is there, print out the value of "test"
+//            Log.d(LOG_TAG, "Contains: " + extras.getString("test"));
+//        }
 
 
         // Get token from the ID Service you created and show it in a log
