dd29974a3478d895e7b3a61ababb4bf8abf10a6a
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-11-04 17:27:06 +0300

    ShushMe project update T0X.05 finished

--- Adv04/Shushme.T0X.05/app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
+++ Adv04/Shushme.T0X.05/app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
@@ -16,10 +16,16 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Build;
 import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
@@ -35,14 +41,36 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
      */
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "onReceive called");
+//        Log.i(TAG, "onReceive called");
         // TODO (4) Use GeofencingEvent.fromIntent to retrieve the GeofencingEvent that caused the transition
-
         // TODO (5) Call getGeofenceTransition to get the transition type and use AudioManager to set the
         // phone ringer mode based on the transition type. Feel free to create a helper method (setRingerMode)
-
         // TODO (6) Show a notification to alert the user that the ringer mode has changed.
         // Feel free to create a helper method (sendNotification)
+        if(intent == null) {
+            return;
+        }
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        int geoFenciesTransition = geofencingEvent.getGeofenceTransition();
+        if(geoFenciesTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            setRingerMode(context,AudioManager.RINGER_MODE_SILENT);
+            Log.i(TAG, "onReceive Geofencies transition enter");
+        }else if(geoFenciesTransition == Geofence.GEOFENCE_TRANSITION_EXIT){
+            setRingerMode(context,AudioManager.RINGER_MODE_NORMAL);
+            Log.i(TAG, "onReceive Geofencies transition exit");
+        }else {
+            Log.e(TAG,context.getString(R.string.transitions_error,geoFenciesTransition));
+        }
+    }
 
+    private void setRingerMode(Context context, int mode) {
+        NotificationManager nm = (NotificationManager)context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT < 24 ||
+                (Build.VERSION.SDK_INT >= 24 && !nm.isNotificationPolicyAccessGranted())) {
+            AudioManager audioManager = (AudioManager)context
+                    .getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setMode(mode);
+        }
     }
 }
--- Adv04/Shushme.T0X.05/app/src/main/java/com/example/android/shushme/MainActivity.java
+++ Adv04/Shushme.T0X.05/app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -16,13 +16,16 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.app.NotificationManager;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -247,6 +250,16 @@ public class MainActivity extends AppCompatActivity implements
         }
 
         //TODO (3) Initialize ringer permissions checkbox
+
+        CheckBox ringerPermissions = (CheckBox) findViewById(R.id.ringer_permission_checkbox);
+        NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= 24 && !nm.isNotificationPolicyAccessGranted()) {
+            ringerPermissions.setChecked(false);
+        } else {
+            ringerPermissions.setChecked(true);
+            ringerPermissions.setEnabled(false);
+        }
+
     }
 
     // TODO (2) Implement onRingerPermissionsClicked to launch ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS
@@ -256,4 +269,12 @@ public class MainActivity extends AppCompatActivity implements
                 new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                 PERMISSIONS_REQUEST_FINE_LOCATION);
     }
+
+    public void onRingerPermissionClicked(View view) {
+        Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+        startActivity(intent);
+    }
+
+
+
 }
