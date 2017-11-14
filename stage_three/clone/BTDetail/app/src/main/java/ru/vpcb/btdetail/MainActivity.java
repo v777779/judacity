9686b836abeb325ad2c7f02fa34045de24e19af1
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-11-14 21:06:51 +0300

    added BTDetail project home up icon added

--- stage_three/clone/BTDetail/app/src/main/java/ru/vpcb/btdetail/MainActivity.java
+++ stage_three/clone/BTDetail/app/src/main/java/ru/vpcb/btdetail/MainActivity.java
@@ -3,6 +3,7 @@ package ru.vpcb.btdetail;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);// обязательно без Manifest.PARENT
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_home_white_24dp);
+        }
+
+
         setContentView(R.layout.fragment_main);  // dynamyc version container
         if (savedInstanceState == null) {
             FragmentMain mainFragment = new FragmentMain();
@@ -20,5 +29,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        if(item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- stage_three/clone/BTPlay/app/src/main/java/ru/vpcb/btplay/MainActivity.java
+++ stage_three/clone/BTPlay/app/src/main/java/ru/vpcb/btplay/MainActivity.java
@@ -2,6 +2,7 @@ package ru.vpcb.btplay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);// обязательно без Manifest.PARENT
+        }
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
