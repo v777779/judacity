0ff458a6603711e08ed15dd0a673dc81bab007fb
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-11-17 21:09:50 +0300

    added BTPlay project update recycler expand main connection finished

--- stage_three/clone/BTPlay/app/src/main/java/ru/vpcb/btplay/FragmentMain.java
+++ stage_three/clone/BTPlay/app/src/main/java/ru/vpcb/btplay/FragmentMain.java
@@ -10,12 +10,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -44,9 +45,13 @@ public class FragmentMain extends Fragment implements IFragmentHelper,
     private List<String> mList;
     private RecyclerView mRecyclerView;
     private FragmentMainAdapter mRecyclerAdapter;
+    private ProgressBar mProgressBar;
+    private TextView mErrorMessage;
 
     private LoaderUri mLoader;
     private LoaderDb mLoaderDb;
+
+
 
     // test!!!
     private Toast mToast;
@@ -60,13 +65,9 @@ public class FragmentMain extends Fragment implements IFragmentHelper,
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         mLoader = new LoaderUri(getContext(), this);
         mLoaderDb = new LoaderDb(getContext(), this);
-
         if (NetworkData.isOnline(getContext())) {
             getLoaderManager().initLoader(LOADER_RECIPES_ID, new Bundle(), mLoader); // empty bundle FFU
-        } else {
-            showError();
         }
-
         getLoaderManager().initLoader(LOADER_RECIPES_DB_ID, null, mLoaderDb); // empty bundle FFU
 
         super.onActivityCreated(savedInstanceState);
@@ -89,30 +90,42 @@ public class FragmentMain extends Fragment implements IFragmentHelper,
 
         mRecyclerAdapter = new FragmentMainAdapter(rootView.getContext(), this);     //context  and data
         mRecyclerView.setAdapter(mRecyclerAdapter);
+        mProgressBar = rootView.findViewById(R.id.progress_bar);
+        mErrorMessage = rootView.findViewById(R.id.error_message);
+
+// loaders
+
+         if (!NetworkData.isOnline(getContext())) {
+                   showError();
+        }
 
         return rootView;
     }
 
-    private static final int LOW_WIDTH_PORTRAIT = 400;   //ok
+    private static final int LOW_WIDTH_PORTRAIT = 350;   //ok
     private static final int LOW_WIDTH_LANDSCAPE = 550;  //ok
     private static final int LOW_SCALE_PORTRAIT = 250;   //ok
     private static final int LOW_SCALE_LANDSCAPE = 200;  // ok
 
-    private static final int MIDDLE_WIDTH_PORTRAIT = 600;
-    private static final int MIDDLE_WIDTH_LANDSCAPE = 850;
-    private static final int MIDDLE_SCALE_PORTRAIT = 200;
+    private static final int MIDDLE_WIDTH_PORTRAIT = 400;
+    private static final int MIDDLE_WIDTH_LANDSCAPE = 600;
+    private static final int MIDDLE_SCALE_PORTRAIT = 250;
     private static final int MIDDLE_SCALE_LANDSCAPE = 250;
 
-    private static final int HIGH_WIDTH_PORTRAIT = 800;
+    private static final int HIGH_WIDTH_PORTRAIT = 750;
     private static final int HIGH_WIDTH_LANDSCAPE = 1200;
     private static final int HIGH_SCALE_PORTRAIT = 300;
-    private static final int HIGH_SCALE_LANDSCAPE = 350;
+    private static final int HIGH_SCALE_LANDSCAPE = 300;
 
     private static final int MAX_SPAN = 6;
     private static final int MIN_SPAN = 1;
     private static final int MIN_HEIGHT = 200;
-    private static final double SCALE_RATIO_PORTRAIT = 1.8;  // low_ok
-    private static final double SCALE_RATIO_LANDSCSAPE = 2.3; // low_ok
+    private static final double LOW_RATIO_PORTRAIT = 1.8;  // low_ok
+    private static final double LOW_RATIO_LANDSCSAPE = 1.8; // low_ok
+    private static final double MIDDLE_RATIO_PORTRAIT = 1.8;  // low_ok
+    private static final double MIDDLE_RATIO_LANDSCSAPE = 1.8; // low_ok
+    private static final double HIGH_RATIO_PORTRAIT = 1.8;  // low_ok
+    private static final double HIGH_RATIO_LANDSCSAPE = 1.8; // low_ok
 
     private int mSpan;
     private int mSpanHeight;
@@ -121,36 +134,40 @@ public class FragmentMain extends Fragment implements IFragmentHelper,
         DisplayMetrics dp = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(dp);
         boolean isLand = dp.widthPixels > dp.heightPixels;
-        int width = (int)(dp.widthPixels/dp.density);
+        int width = (int) (dp.widthPixels / dp.density);
 
-        if(!isLand) {
+        if (!isLand) {
             mSpan = 1;
-            if(width > HIGH_WIDTH_PORTRAIT) {
-                mSpanHeight = (int)(HIGH_WIDTH_PORTRAIT/SCALE_RATIO_PORTRAIT);
-                mSpan = (int)(HIGH_WIDTH_PORTRAIT/HIGH_SCALE_PORTRAIT);
-            }else if (width > MIDDLE_WIDTH_PORTRAIT) {
-                mSpanHeight = (int) (MIDDLE_WIDTH_PORTRAIT / SCALE_RATIO_PORTRAIT);
-                mSpan = (int)(MIDDLE_WIDTH_PORTRAIT/MIDDLE_SCALE_PORTRAIT);
-            }else {
-                mSpanHeight = (int) (LOW_WIDTH_PORTRAIT / SCALE_RATIO_PORTRAIT);
-                mSpan = (int)(LOW_WIDTH_PORTRAIT/LOW_SCALE_PORTRAIT);
+            if (width > HIGH_WIDTH_PORTRAIT) {
+                mSpan = (int) (HIGH_WIDTH_PORTRAIT / HIGH_SCALE_PORTRAIT);
+                mSpanHeight = (int) (dp.widthPixels / mSpan / HIGH_RATIO_PORTRAIT);
+
+            } else if (width > MIDDLE_WIDTH_PORTRAIT) {
+                mSpan = (int) (MIDDLE_WIDTH_PORTRAIT / MIDDLE_SCALE_PORTRAIT);
+                mSpanHeight = (int) (dp.widthPixels / mSpan / MIDDLE_RATIO_PORTRAIT);
+
+            } else {
+                mSpan = (int) (LOW_WIDTH_PORTRAIT / LOW_SCALE_PORTRAIT);
+                mSpanHeight = (int) (dp.widthPixels / mSpan / LOW_RATIO_PORTRAIT);
             }
-        }else {
-            if(width > HIGH_WIDTH_LANDSCAPE) {
-                mSpanHeight = (int)(HIGH_WIDTH_LANDSCAPE/SCALE_RATIO_LANDSCSAPE);
-                mSpan = (int)(HIGH_WIDTH_LANDSCAPE/HIGH_SCALE_LANDSCAPE);
-            }else if (width > MIDDLE_WIDTH_LANDSCAPE) {
-                mSpanHeight = (int) (MIDDLE_WIDTH_LANDSCAPE / SCALE_RATIO_LANDSCSAPE);
-                mSpan = (int)(MIDDLE_WIDTH_LANDSCAPE/MIDDLE_SCALE_LANDSCAPE);
-            }else {
-                mSpanHeight = (int) (LOW_WIDTH_LANDSCAPE / SCALE_RATIO_LANDSCSAPE);
-                mSpan = (int)(LOW_WIDTH_LANDSCAPE/LOW_SCALE_LANDSCAPE);
+        } else {
+            if (width > HIGH_WIDTH_LANDSCAPE) {
+                mSpan = (int) (HIGH_WIDTH_LANDSCAPE / HIGH_SCALE_LANDSCAPE);
+                mSpanHeight = (int) (dp.widthPixels / mSpan / HIGH_RATIO_LANDSCSAPE);
+
+            } else if (width > MIDDLE_WIDTH_LANDSCAPE) {
+
+                mSpan = (int) (MIDDLE_WIDTH_LANDSCAPE / MIDDLE_SCALE_LANDSCAPE);
+                mSpanHeight = (int) (dp.widthPixels / mSpan / MIDDLE_RATIO_LANDSCSAPE);
+            } else {
+                mSpan = (int) (LOW_WIDTH_LANDSCAPE / LOW_SCALE_LANDSCAPE);
+                mSpanHeight = (int) (dp.widthPixels / mSpan / LOW_RATIO_LANDSCSAPE);
             }
         }
 
-        if(mSpan < MIN_SPAN) mSpan = MIN_SPAN;
-        if(mSpan >MAX_SPAN)mSpan = MAX_SPAN;
-        if(mSpanHeight < MIN_HEIGHT) mSpanHeight = MIN_HEIGHT;
+        if (mSpan < MIN_SPAN) mSpan = MIN_SPAN;
+        if (mSpan > MAX_SPAN) mSpan = MAX_SPAN;
+        if (mSpanHeight < MIN_HEIGHT) mSpanHeight = MIN_HEIGHT;
 
     }
 
@@ -212,42 +229,56 @@ public class FragmentMain extends Fragment implements IFragmentHelper,
 
     @Override
     public void showProgress() {
-        showToast("Main Progress");
+//        showToast("Main Progress");
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void showError() {
+//        showToast("Main Error");
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    private void showResult() {
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mErrorMessage.setVisibility(View.INVISIBLE);
+
     }
 
     @Override
     public void onComplete(Bundle data) {
+        showResult();
+
         String s = null;
         if (data != null) {
             s = data.getString(BUNDLE_LOADER_STRING_ID);
         }
         final List<RecipeItem> listRecipeItem = RecipeItem.getRecipeList(s);
 
-        showToast("Main Complete");
+//        showToast("Main Complete");
 
-        new AsyncTask<Void, Void,Integer>() {
+        new AsyncTask<Void, Void, Integer>() {
             @Override
             protected Integer doInBackground(Void... params) {
                 return RecipeData.bulkInsert(getActivity().getContentResolver(),
-                        getLoaderManager(),listRecipeItem, mLoaderDb);
+                        getLoaderManager(), listRecipeItem, mLoaderDb);
             }
         }.execute();
     }
 
-    @Override
-    public void showError() {
-        showToast("Main Error");
-    }
 
     @Override
     public void onComplete(Cursor cursor) {
-        if (cursor == null || mRecyclerAdapter == null) {   // нет адаптера выходим
-            showToast("Main No Database");
+        if (cursor == null || cursor.getCount() == 0 || mRecyclerAdapter == null ) {   // нет адаптера выходим
             return;
         }
         mRecyclerAdapter.swapCursor(cursor);
-        showToast("Main Database Complete");
-
+       showResult(); // только после загрузки базы данных
+        if(!NetworkData.isOnline(getContext()))  {
+            Snackbar.make(getView(),"No connection. Local database used",
+                    BaseTransientBottomBar.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -261,7 +292,7 @@ public class FragmentMain extends Fragment implements IFragmentHelper,
         if (mToast != null) {
             mToast.cancel();
         }
-        Snackbar.make(getView(),s, BaseTransientBottomBar.LENGTH_SHORT).show();
+        Snackbar.make(getView(), s, BaseTransientBottomBar.LENGTH_SHORT).show();
 //        mToast = Toast.makeText(getContext(), s, Toast.LENGTH_SHORT);
 //        mToast.show();
 
