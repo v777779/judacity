b371fd3939755a07b023c208ce4a24d9fdc94637
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-12-02 14:51:05 +0300

    project Baking App update fragmenterror jdoc updated

--- stage_three/BakingApp/app/src/main/java/ru/vpcb/bakingapp/FragmentError.java
+++ stage_three/BakingApp/app/src/main/java/ru/vpcb/bakingapp/FragmentError.java
@@ -11,21 +11,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+
 /**
- * Created by V1 on 29-Nov-17.
+ *  FragmentError is Dialog Fragment class
+ *  to show dialog when there is no connection to network
  */
-
 public class FragmentError extends DialogFragment implements View.OnClickListener {
-    final String LOG_TAG = "myLogs";
-    private boolean isFinish = false;
     private IFragmentHelper mCallback;
-
     private int mLayoutId;
 
+    /**
+     * Constructor default
+     *  Sets default layout for MainActivity
+     */
     public FragmentError() {
         this.mLayoutId = R.layout.fragment_error;
     }
 
+    /**
+     *  Creates FragmentError main View
+     *  Setup different layouts using mLayoutId field
+     *  Setup different titile for MainActivity and DetailsActivity
+     *
+     *
+     * @param inflater    LayoutInflater inflates layout
+     * @param container ViewGroup parent view
+     * @param savedInstanceState  Bundle savedInstanceState
+     * @return View of dialog
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -39,21 +52,36 @@ public class FragmentError extends DialogFragment implements View.OnClickListene
         } else {
             getDialog().setTitle(getString(R.string.empty_title));
         }
-
-
         return v;
     }
 
+    /**
+     *  Set  layout ID of dialog
+     *  Called from MainActivity and DetailActivity to inflate different layouts
+     *
+     * @param mLayoutId
+     */
     public void setLayoutId(int mLayoutId) {
         this.mLayoutId = mLayoutId;
     }
 
+    /**
+     *  Set callback IFragmentHelper object
+     *  Used to call showError() method in MainActivity onClick()
+     *
+     * @param mCallback IFragmentHelper callback object
+     */
     public void setCallback(IFragmentHelper mCallback) {
         this.mCallback = mCallback;
     }
 
+    /**
+     *  OnClick method, recreate MainActivity on RETRY or finish() any activity on EXIT.
+     *
+     *
+     * @param v  View object of button
+     */
     public void onClick(View v) {
-        Log.d(LOG_TAG, "Dialog 1: " + ((Button) v).getText());
         String s = ((Button) v).getText().toString();
         if (s.equals(getString(R.string.error_retry))) {
             recreate();
@@ -68,30 +96,12 @@ public class FragmentError extends DialogFragment implements View.OnClickListene
 
         }
         dismiss();
-
-
     }
 
 
-    public void onDismiss(DialogInterface dialog) {
-        super.onDismiss(dialog);
-        Log.d(LOG_TAG, "Dialog 1: onDismiss*");
-
-
-    }
-
-    public void onCancel(DialogInterface dialog) {
-        super.onCancel(dialog);
-        Log.d(LOG_TAG, "Dialog 1: onCancel");
-        isFinish = true;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-
-    }
-
+    /**
+     *  Recreates activity with SDK version support.
+     */
     private void recreate() {
         Activity parent = getActivity();
         if (android.os.Build.VERSION.SDK_INT >= 11) {
