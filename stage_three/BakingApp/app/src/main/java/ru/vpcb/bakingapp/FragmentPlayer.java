760a86eb17e98f4463ab27b6274dae59e2b5d16a
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-12-02 16:12:45 +0300

    project Baking App update fragment player jdoc added

--- stage_three/BakingApp/app/src/main/java/ru/vpcb/bakingapp/FragmentPlayer.java
+++ stage_three/BakingApp/app/src/main/java/ru/vpcb/bakingapp/FragmentPlayer.java
@@ -2,6 +2,7 @@ package ru.vpcb.bakingapp;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -63,65 +64,186 @@ import static ru.vpcb.bakingapp.utils.Constants.SYSTEM_UI_SHOW_FLAGS;
  * Date: 15-Nov-17
  * Email: vadim.v.voronov@gmail.com
  */
-public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoEventCallback {
 
+/**
+ * FragmentPlayer class shows Fragment  with Recipe.Step data
+ */
+public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoEventCallback {
+    /**
+     * The text with Step name
+     */
     @Nullable
     @BindView(R.id.fp_head_text)
     TextView mHeadText;
+    /**
+     * The text with Step description
+     */
     @Nullable
     @BindView(R.id.fp_body_text)
     TextView mBodyText;
+
+    /**
+     * Bottom left navigation button
+     */
     @Nullable
     @BindView(R.id.prev_button)
     ImageView mPrevButton;
+    /**
+     * Bottom right navigation button
+     */
     @Nullable
     @BindView(R.id.next_button)
     ImageView mNextButton;
+
+
+    /**
+     * The text with Step name between buttons
+     */
     @Nullable
     @BindView(R.id.navigation_text)
     TextView mNavigationText;
+
+    /**
+     * FrameLayout that extends clickable area of left button
+     */
     @Nullable
     @BindView(R.id.prev_button_extended)
     View mPrevExt;
+    /**
+     * FrameLayout that extends clickable area of right button
+     */
     @Nullable
     @BindView(R.id.next_button_extended)
     View mNextExt;
+
+    /**
+     * ExoPlayer View object
+     */
     @Nullable
     @BindView(R.id.exoplayer_view)
     SimpleExoPlayerView mPlayerView;
+
+    /**
+     * ExoPlayer Play button Overlay Image
+     */
     @Nullable
     @BindView(R.id.ic_play_button)
     ImageView mPlayButton;
+    /**
+     * ExoPlayer Play Button Background Overlay Image
+     */
     @Nullable
     @BindView(R.id.ic_play_button_back)
     ImageView mPlayButtonBack;
+    /**
+     * ExoPlayer Placeholder Image.
+     */
     @Nullable
     @BindView(R.id.fp_video_card)
     View mCardView;
 
+    /**
+     * Current context of activity.
+     */
     private Context mContext;
+    /**
+     * The RecipeItem parent object.
+     */
     private RecipeItem mRecipeItem;
+    /**
+     * The List of RecipeItem Step objects.
+     */
     private List<RecipeItem.Step> mStepList;
+    /**
+     * The current RecipeItem Step object.
+     */
     private RecipeItem.Step mCurrentStep;
+    /**
+     * The position of current RecipeItem Step object in List.
+     */
     private int mPosition;
+    /**
+     * The maximum position of RecipeItem Step object in List
+     */
     private int mPositionMax;
+
+    /**
+     * The flag if screen smallest wide greater or even 550dp (true) or less (false)
+     */
     private boolean mIsWide;
+
+    /**
+     * The flag is true when playback was ended
+     */
     private boolean mPlaybackEnded;
+    /**
+     * The current position of player while playing video
+     */
     private long mPlaybackPosition;
+    /**
+     * The flag of play or pause mode, is tru when player is playing
+     */
     private boolean mPlaybackWhenReady;
+
+    /**
+     * The value of current window of player
+     */
     private int mCurrentWindow;
+    /**
+     * ExoPlayer object
+     */
     private SimpleExoPlayer mPlayer;
+    /**
+     * ExoPlayer Player.EventListener object
+     */
     private VideoEventListener mVideoListener;
+    /**
+     * The string with URL of video source
+     */
     private String mVideoURL;
+    /**
+     * The flag is true when video is enabled.
+     * Video is enabled if connection to network exists and mVideoURL value is valid.
+     */
     private boolean mIsVideoEnabled;
+    /**
+     * The flag is true if device orientation is landscape.
+     * Used to set FULL_SCREEN mode for devices with smallest screen wide less than 550dp
+     */
     private boolean mIsLandMode;
+    /**
+     * ButterKnife object, used to close all binds on destroy.
+     */
     private Unbinder mUnBinder;
 
-
+    /**
+     * Constructor default.
+     */
     public FragmentPlayer() {
-
     }
 
+    /**
+     * Create main View of FragmentPlayer.
+     * Inflates layout of Fragment.
+     * Extracts parameters from input Intent bundle.
+     * Fill layout with parameters from Intent bundle.
+     * Starts ExoPlayer playback.
+     * Bundle parameters:<br>
+     * mPosition    the position of current Step in List of steps.
+     * mIsWide      the flag of current screen, true if screen is wide.
+     * mRecipeItem  the parent RecipeItem object.
+     * *
+     *
+     * @param inflater           LayoutInflater inflates layout of fragment
+     * @param container          ViewGroup parent view
+     * @param savedInstanceState Bundle with instance parameters
+     *                           Bundle parameters:<br>
+     *                           mCurrentWindow   int value used by ExoPlayer
+     *                           mPlaybackPosition long value used by ExoPlayer
+     *                           mPlaybackWhenReady  boolean flag shows if player is in play or pause mode
+     *                           mPlaybackEnded boolean flag shows if playback was ended
+     * @return View object of fragment
+     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -163,7 +285,7 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
 // text
         mCurrentStep = getCurrentStep();
         if (mHeadText != null && mRecipeItem != null) {
-            mHeadText.setText(getRecipeName(mContext.getResources(),mRecipeItem));
+            mHeadText.setText(getRecipeName(mContext.getResources(), mRecipeItem));
         }
         setupNavTextView();
         setupNavButtons();
@@ -175,7 +297,12 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         return rootView;
     }
 
-
+    /**
+     * Attaches parent activity context to this fragment.
+     * Used to fill mContext with parent activity context.
+     *
+     * @param context
+     */
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -187,6 +314,16 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         mContext = context;
     }
 
+    /**
+     * Saves instance data to Bundle object
+     * Bundle parameters:<br>
+     * mCurrentWindow   int value used by ExoPlayer
+     * mPlaybackPosition long value used by ExoPlayer
+     * mPlaybackWhenReady  boolean flag shows if player is in play or pause mode
+     * mPlaybackEnded boolean flag shows if playback was ended
+     *
+     * @param outState
+     */
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -246,7 +383,6 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         if (!mIsLandMode || mIsWide) {
             return;
         }
-
         if (!mIsVideoEnabled) {
             getView().setSystemUiVisibility(SYSTEM_UI_HIDE_FLAGS);
         } else {
@@ -254,6 +390,12 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         }
     }
 
+    /**
+     * Returns MediaSource object for input URL parameter
+     *
+     * @param sourceURI String  media URL
+     * @return MediaSource object
+     */
     private MediaSource buildMediaSource(String sourceURI) {
         MediaSource mediaSource = new ExtractorMediaSource(
                 Uri.parse(sourceURI),
@@ -264,6 +406,10 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         return mediaSource;
     }
 
+    /**
+     * Checks if video is possible release ExoPlayer if not.
+     * Set mIsVideoEnabled flag true if video is enabled.
+     */
     private void setVideoAccess() {
         if (mCurrentStep == null || mCurrentStep.getVideoURL() == null ||
                 mCurrentStep.getVideoURL().isEmpty() || !isOnline(mContext)) {
@@ -282,6 +428,9 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         mIsVideoEnabled = true;
     }
 
+    /**
+     * Initializes ExoPlayer object and attach Player.EventListener to ExoPlayer.
+     */
     private void initializePlayer() {
         if (!mIsVideoEnabled) {
             return;
@@ -304,6 +453,9 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         mPlayer.prepare(mediaSource, true, false);
     }
 
+    /**
+     * Releases ExoPlayer object when video playing is not accessible or on stop of fragment activity.
+     */
     private void releasePlayer() {
         if (mPlayer != null) {
             mPlaybackPosition = mPlayer.getContentPosition();
@@ -319,6 +471,14 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         }
     }
 
+    /**
+     * Callback method from IFragmentHelper interface object.
+     * Replaces current FragmentPlayer when button prev or next clicked.
+     * Used for not wide screens only, when there is FragmentPlayer on screen only.
+     * And there are prev and next buttons in the bottom of layout.
+     *
+     * @param position int current position of Step object in List of RecipeItem Step
+     */
     @Override
     public void onCallback(int position) {
         if (mIsWide) {
@@ -333,26 +493,25 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         playerArgs.putBoolean(RECIPE_SCREEN_WIDE, mIsWide);
         playerFragment.setArguments(playerArgs);
         fragmentManager.popBackStack(FRAGMENT_PLAYER_NAME, POP_BACK_STACK_INCLUSIVE);
-
-        if (mIsWide) {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.fc_p_container, playerFragment)
-                    .commit();
-        } else {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, playerFragment)
-                    .addToBackStack(FRAGMENT_PLAYER_NAME)
-                    .commit();
-        }
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, playerFragment)
+                .addToBackStack(FRAGMENT_PLAYER_NAME)
+                .commit();
 
     }
 
+    /**
+     *  PlaceHolder for IFragmentHelper interface
+     */
     @Override
     public void showError() {
 
     }
 
-
+    /**
+     *  Setup visibility if navigation buttons according to position
+     *  In the first Step of the list the prev button is invisible, in the last Step the next one.
+     */
     private void setupNavButtonsVisibility() {
         if (mPosition <= 1) {
             mPrevButton.setVisibility(View.INVISIBLE);
@@ -366,6 +525,11 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         }
     }
 
+    /**
+     *  Setup navigation button callback methods
+     *  onCallback() method the current FragmentPlayer replaced by the new one
+     *  Checks and sets visibility of navigation buttons
+     */
     private void setupNavButtons() {
         if (mPrevExt == null || mNextExt == null) {
             return;
@@ -396,6 +560,9 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         });
     }
 
+    /**
+     *  Sets navigation bar text according to Step data
+     */
     private void setupNavTextView() {
         if (mNavigationText == null) {
             return;
@@ -404,10 +571,14 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
             mNavigationText.setText(getString(R.string.play_header_empty));
 
         } else {
-            mNavigationText.setText(getStepName(mContext.getResources(),mCurrentStep));
+            mNavigationText.setText(getStepName(mContext.getResources(), mCurrentStep));
         }
     }
 
+    /**
+     * Sets Step description text
+     *
+     */
     private void setupBodyTextView() {
         if (mBodyText == null) {
             return;
@@ -415,7 +586,7 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         if (mCurrentStep == null) {
             mBodyText.setText(getString(R.string.play_body_error));
         } else {
-            String stepText = getDescription(mContext.getResources(),mCurrentStep);
+            String stepText = getDescription(mContext.getResources(), mCurrentStep);
             if (stepText == null || stepText.isEmpty()) {
                 stepText = getString(R.string.play_body_empty);
             }
@@ -423,6 +594,11 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
         }
     }
 
+    /**
+     *  Returns current Step object by the mPosition value
+     *
+     * @return RecipeItem.Step object
+     */
     private RecipeItem.Step getCurrentStep() {
         if (mStepList == null || mPosition == 0 || mPosition > mStepList.size()) {
             return null;
@@ -431,6 +607,11 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
     }
 
 
+    /**
+     *  Sets ExoPlayer Overlay Image of Play Button visibility
+     *  The overlay image is visible if video is Ended or not started.
+     *  Performs smooth transition between states of visibility
+     */
     private void setupPlayButton() {
         if (!mIsVideoEnabled) {
             return;
@@ -461,34 +642,44 @@ public class FragmentPlayer extends Fragment implements IFragmentHelper, IVideoE
                             .alpha(0f)
                             .setDuration(PLAY_BUTTON_ANIMATION)
                             .setListener(null);
-
                 }
             }
         });
 
     }
 
+//    private boolean isLandMode() {
+//        return Configuration.ORIENTATION_LANDSCAPE == getResources().getConfiguration().orientation;
+//    }
 
+    /**
+     *  Returns current screen orientation mode
+     *
+     * @return boolean flag of orientation, true if orientation is landscape
+     */
     private boolean isLandMode() {
         DisplayMetrics dp = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(dp);
         return dp.widthPixels > dp.heightPixels;
     }
 
+    /**
+     * Callback for ExoPlayer Player.EventListener.
+     * Checks STATE_READY and  STATE_ENDED states of player
+     * and sets visibility of overlay image
+     *
+     * @param playbackState
+     */
     @Override
     public void setPlayState(int playbackState) {
 
         if (playbackState == Player.STATE_READY) {
             mPlaybackEnded = false;
-
-//            if (!mPlaybackWhenReady) mPlayButton.setImageResource(R.drawable.ic_play_circle_24dp);
-//            else mPlayButton.setImageResource(R.drawable.ic_pause_circle_24dp);
         }
 
         mPlayButton.setVisibility(View.VISIBLE);
         if (playbackState == Player.STATE_ENDED) {
             mPlaybackWhenReady = false;
-//            mPlayButton.setImageResource(R.drawable.ic_play_circle_24dp);
             mPlayButton.setAlpha(1f);
             mPlaybackEnded = true;
             mPlayerView.setControllerAutoShow(false);
