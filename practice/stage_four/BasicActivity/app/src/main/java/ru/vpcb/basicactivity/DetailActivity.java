21619cb956963ba858a2f45eec23177fb2ba5832
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-12-10 15:09:29 +0300

    project BasicActivity update moved to Android Version 3.0 and normal endpoints implementation

---
+++ practice/stage_four/BasicActivity/app/src/main/java/ru/vpcb/basicactivity/DetailActivity.java
@@ -0,0 +1,47 @@
+package ru.vpcb.basicactivity;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static ru.vpcb.basicactivity.MainActivity.INTENT_STRING_EXTRA;
+
+public class DetailActivity extends AppCompatActivity {
+    private TextView mTextView;
+    private FloatingActionButton mFabBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_home_white_24dp);
+        }
+
+        mTextView = findViewById(R.id.detail_text);
+        mFabBack = findViewById(R.id.fab_back);
+        mFabBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+//                overridePendingTransition(R.anim.slide_in_main, R.anim.slide_out_main);
+                overridePendingTransition(R.anim.slide_left, R.anim.slide_right_out);
+            }
+        });
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(INTENT_STRING_EXTRA)) {
+            String s = intent.getStringExtra(INTENT_STRING_EXTRA);
+            if (s == null) s = "";
+            mTextView.setText(s);
+        }
+
+    }
+}
---
+++ practice/stage_four/BasicActivity/app/src/main/java/ru/vpcb/basicactivity/ICallback.java
@@ -0,0 +1,9 @@
+package ru.vpcb.basicactivity;
+
+/**
+ * Created by V1 on 09-Dec-17.
+ */
+
+public interface ICallback {
+    void onComplete(String s);
+}
