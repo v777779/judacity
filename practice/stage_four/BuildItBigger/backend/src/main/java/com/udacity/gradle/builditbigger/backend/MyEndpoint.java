41621687f5b4aee7eac860c5066c36a2da7b1170
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-12-15 12:11:09 +0300

    project BuildItBigger practice init

---
+++ practice/stage_four/BuildItBigger/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -0,0 +1,50 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.udacity.gradle.builditbigger.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import javax.inject.Named;
+
+import ru.vpcb.jokeprovider.JokeProvider;
+
+import static ru.vpcb.constants.Constants.REQUEST_TEST_GET_TEMPLATE;
+import static ru.vpcb.constants.Constants.REQUEST_TEST_OUT_TEMPLATE;
+
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.builditbigger.gradle.udacity.com",
+                ownerName = "backend.builditbigger.gradle.udacity.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        if(name != null && name.toLowerCase().equals(REQUEST_TEST_GET_TEMPLATE)) {
+            response.setData(REQUEST_TEST_OUT_TEMPLATE);
+        }
+        else {
+            response.setData(new JokeProvider().getJoke());
+        }
+        return response;
+    }
+
+}
---
+++ practice/stage_four/BuildItBigger/constants/src/main/java/ru/vpcb/constants/Constants.java
@@ -0,0 +1,44 @@
+package ru.vpcb.constants;
+
+/**
+ * Exercise for course : Android Developer Nanodegree
+ * Created: Vadim Voronov
+ * Date: 14-Dec-17
+ * Email: vadim.v.voronov@gmail.com
+ */
+
+public class Constants {
+
+    // main activity
+    public static final String INTENT_STRING_EXTRA = "intent_string_extra";
+    public static final String INTENT_IMAGE_EXTRA = "intent_image_extra";
+
+    public static final String REQUEST_GET_TEMPLATE = "get";
+    public static final String REQUEST_TEST_GET_TEMPLATE = "test";
+    public static final String REQUEST_TEST_OUT_TEMPLATE = "test joke received";
+    public static final int CONNECT_TIMEOUT = 5;
+    public static final String MESSAGE_TEST_OK = "*** Endpont Test passed ***";
+    public static final int AD_ACTIVATION_COUNTER = 5;
+
+    // display    public static final String BUNDLE_JOKE_STRING = "bundle_joke_string";
+    public static final String BUNDLE_JOKE_LIST = "bundle_joke_list";
+    public static final String BUNDLE_POSITION = "bundle_position";
+
+    public static final String BUNDLE_FRONT_TEXT_ID = "bundle_front_text_id";
+    public static final String BUNDLE_FRONT_IMAGE_ID = "bundle_front_image_id";
+
+    public static final String BUNDLE_JOKE_TEXT_ID = "bundle_joke_text_id";
+    public static final String BUNDLE_JOKE_IMAGE_ID = "bundle_joke_image_id";
+    public static final String BUNDLE_PROGRESS_BAR = "bundle_progress_bar";
+
+    // recycler
+    public static final int HIGH_SCALE_WIDTH = 240;     // dpi
+    public static final int HIGH_SCALE_HEIGHT = 180;     // dpi
+    public static final double SCALE_RATIO_VERT = 1.5;
+    public static final double SCALE_RATIO_HORZ = 1.8;
+
+    public static final int MIN_SPAN = 1;
+    public static final int MIN_HEIGHT = 100;
+    public static final int MIN_WIDTH = 200;
+
+}
