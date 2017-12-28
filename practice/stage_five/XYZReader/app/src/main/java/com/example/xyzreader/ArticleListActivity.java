3e0eb491a30f4934c0d512da09baa14d14053d81
Vadim Voronov <vadim.v.voronov@gmail.com>
2017-12-28 15:52:32 +0300

    project XYZReader update toolbar click added

--- practice/stage_five/XYZReader/app/src/main/java/com/example/xyzreader/ArticleListActivity.java
+++ practice/stage_five/XYZReader/app/src/main/java/com/example/xyzreader/ArticleListActivity.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,9 +12,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ArticleListActivity extends AppCompatActivity {
+
+    private ImageView mToolbarLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +26,27 @@ public class ArticleListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        toolbar.setBackgroundResource(R.drawable.toolbar_background);
-
-// test!!!
-        ((TextView)findViewById(R.id.main_text)).setText(R.string.demo_text);
-//             getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-
-
-// end test!!!
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mToolbarLogo = toolbar.findViewById(R.id.toolbar_logo);
+        mToolbarLogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onBackPressed();
             }
         });
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+
+
+
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
