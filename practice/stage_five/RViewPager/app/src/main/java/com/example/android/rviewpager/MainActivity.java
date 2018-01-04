83261d1f74540784c8496653c1323dd2b1f3a1ae
Vadim Voronov <vadim.v.voronov@gmail.com>
2018-01-04 20:13:11 +0300

    project RViewPager update multiple shared fab transition finished

--- practice/stage_five/RViewPager/app/src/main/java/com/example/android/rviewpager/MainActivity.java
+++ practice/stage_five/RViewPager/app/src/main/java/com/example/android/rviewpager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.rviewpager;
 
+import android.app.ActivityOptions;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,14 +10,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.transition.Explode;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.GridLayout;
 
 import java.util.ArrayList;
@@ -28,37 +33,75 @@ import butterknife.Unbinder;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity implements ICallback {
+    public static final String BUNDLE_IMAGE_RESOURCE = "bundle_image_resource";
+    private static boolean mIsTimber;
 
     @Nullable
     @BindView(R.id.recycler)
-    RecyclerView  mRecyclerView;
+    RecyclerView mRecyclerView;
+
+    @Nullable
+    @BindView(R.id.fab)
+    FloatingActionButton mFab;
+
+    @Nullable
+    @BindView(R.id.main_text)
+    View mView;
 
     private Unbinder mUnbinder;
     private RecyclerAdapter mRecyclerAdapter;
     private GridLayoutManager mGridLayout;
     private int mSpan;
     private int mHeight;
+    private List<String> mList;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+// set an enter transition
+        getWindow().setEnterTransition(new Explode());
+        getWindow().setSharedElementEnterTransition(new Explode());
+// set an exit transition
+        getWindow().setExitTransition(new Explode());
+        getWindow().setSharedElementExitTransition(new Explode());
+
+// set an reenter transition
+        getWindow().setReenterTransition(new Explode());
+        getWindow().setSharedElementReenterTransition(new Explode());
+
+
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        mUnbinder = ButterKnife.bind(this);
+
+        // timber
+        if (!mIsTimber) {
+            Timber.plant(new Timber.DebugTree());
+            mIsTimber = true;
+        }
+
+
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,DetailActivity.class));
+                startActivity(new Intent(MainActivity.this, DetailActivity.class));
             }
         });
 
-        mUnbinder = ButterKnife.bind(this);
         setupRecycler();
 
         listImagesLoader();
+
+
+
     }
 
     @Override
@@ -105,12 +148,12 @@ public class MainActivity extends AppCompatActivity implements ICallback {
     }
 
     private void listImagesLoader() {
-        List<String> list = new ArrayList<>();
+        mList = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            list.add(String.format("image_%03d.jpg",i+1));
+            mList.add(String.format("image_%03d.jpg", i + 1));
         }
 
-        mRecyclerAdapter.setCursor(list);
+        mRecyclerAdapter.setCursor(mList);
     }
 
     @Override
@@ -119,8 +162,37 @@ public class MainActivity extends AppCompatActivity implements ICallback {
     }
 
     @Override
-    public void onCallback(int position) {
+    public void onCallback(int mode) {
 
-        startActivity(new Intent(this,DetailActivity.class));
+    }
+
+    @Override
+    public void onCallback(View view, int position) {
+
+//        Bundle bundle = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this
+//                , view, getString(R.string.transition_name)).toBundle();
+//        Intent intent = new Intent(this, DetailActivity.class);
+//        intent.putExtra(BUNDLE_IMAGE_RESOURCE, mList.get(position));
+//        startActivity(intent, bundle);
+
+
+        Intent intent = new Intent(this, DetailActivity.class);
+
+        intent.putExtra(BUNDLE_IMAGE_RESOURCE, mList.get(position));
+        View viewFab = (View) findViewById(R.id.fab);
+        View viewText = (View) findViewById(R.id.main_text);
+
+        Pair<View, String> p1 = Pair.create(view, getString(R.string.transition_image));
+        Pair<View, String> p2 = Pair.create(viewFab, getString(R.string.transition_fab));
+        Pair<View, String> p3 = Pair.create(viewText, getString(R.string.transition_text));
+
+
+        ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                this, p1, p2, p3);
+
+
+        startActivity(intent, optionsCompat.toBundle());
+
+
     }
 }
