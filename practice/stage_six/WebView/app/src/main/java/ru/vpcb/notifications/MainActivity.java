4a30c6527c1af01e52ce8f36e7bb24c13bf80c2b
Vadim Voronov <vadim.v.voronov@gmail.com>
2018-02-21 21:18:39 +0300

    project WebView analytics added

--- practice/stage_six/WebView/app/src/main/java/ru/vpcb/notifications/MainActivity.java
+++ practice/stage_six/WebView/app/src/main/java/ru/vpcb/notifications/MainActivity.java
@@ -4,24 +4,35 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.DownloadListener;
-import android.webkit.WebChromeClient;
+
 import android.webkit.WebResourceRequest;
-import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.Toast;
+
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseAnalytics mFirebaseAnalytics2;
+    private Tracker mTracker;
+
+    private int click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +45,26 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Replace with your own action", Toast.LENGTH_LONG).show();
 
                 String URL = "https://www.foxsports.com/arizona/video/1158836803834";
-                openWebPage(URL);
+
+                openWebView(URL);  // webview
+//                openWebPage(URL);  // browser
+
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "fabID");
+                bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "fab_button");
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "WebView Open");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
+// Tracker
+                mTracker.setScreenName("Image~" + " fab");
+                mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+                mTracker.send(new HitBuilders.EventBuilder()
+                        .setCategory("Action")
+                        .setAction("Open22")
+                        .build());
+
 
             }
         });
@@ -46,52 +73,71 @@ public class MainActivity extends AppCompatActivity {
         fab2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                WebView webView = findViewById(R.id.webview);
-                webView.setWebViewClient(new WebViewClient(){   // without client issues an  Exception on readdress
-                    @Override
-                    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                        return false;
-                    }
+                Toast.makeText(MainActivity.this, "Replace with your own action", Toast.LENGTH_LONG).show();
 
-                    @Override
-                    public void onPageFinished(WebView view, String url) {
-                        super.onPageFinished(view, url);
-                        findViewById(R.id.progressBar).setVisibility(View.INVISIBLE);
-                    }
-                });
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "fab2");
+                bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "fab2_button");
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "Toast Message Show");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
 
+                mTracker.setScreenName("Button: " + " fab2");
+                mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+                mTracker.send(new HitBuilders.EventBuilder()
+                        .setCategory("Action")
+                        .setAction("Write22")
+                        .build());
 
-                webView.setDownloadListener(new DownloadListener() {
-
-                    @Override
-                    public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
-
-                    }
-                });
-
-
-                String URL = "https://www.foxsports.com/arizona/video/1158836803834";
-                webView.loadUrl(URL);
-
-                findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
-
-//                String summary = "<html><body>You scored <b>192</b> points.</body></html>";
-//                webView.loadData(summary,"text/html",null);
-
-// raw
-//                URL = "file:///android_res/raw/yandex.htm";
-//                webView.loadUrl(URL);
-
-// check webClient on readdress
-//                URL = "file:///android_res/raw/help.htm";
-//                webView.loadUrl(URL);
-
-
-// assets
-//                URL = "file:///android_asset/index.html";
-//                webView.loadUrl(URL);
             }
         });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "button");
+                bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "buttonName");
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "buttonImage");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+// tracker
+                mTracker.send(new HitBuilders.EventBuilder()
+                        .setCategory("Action")
+                        .setAction("Share232")
+                        .build());
+
+            }
+        });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "button2");
+                bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Clicks:" + click++);
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "buttonImage2");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
+                mTracker.send(new HitBuilders.EventBuilder()
+                        .setCategory("Action")
+                        .setAction("Settings2332323")
+                        .build());
+
+            }
+        });
+
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        mFirebaseAnalytics2 = FirebaseAnalytics.getInstance(this);
+
+
+        // tracker
+        // Obtain the shared Tracker instance.
+        AnalyticsApplication application = (AnalyticsApplication) getApplication();
+        mTracker = application.getDefaultTracker();
+
 
     }
 
@@ -117,6 +163,46 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    // methods
+
+    private void openWebView(String URL) {
+        android.webkit.WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient() {   // without client issues an  Exception on readdress
+            @Override
+            public boolean shouldOverrideUrlLoading(android.webkit.WebView view, WebResourceRequest request) {
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(android.webkit.WebView view, String url) {
+                super.onPageFinished(view, url);
+                findViewById(R.id.progressBar).setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+        webView.setDownloadListener(new DownloadListener() {
+
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+
+            }
+        });
+
+        URL = "https://www.foxsports.com/arizona/video/1158836803834";
+        webView.loadUrl(URL);
+
+        findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "id");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "name");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+    }
+
+
     private void openWebPage(String sURL) {
         Uri webURI = Uri.parse(sURL);
         Intent intent = new Intent(Intent.ACTION_VIEW, webURI);
@@ -133,11 +219,11 @@ public class MainActivity extends AppCompatActivity {
         int value;
         try {
             value = raw.read();
-            while(value != -1) {
+            while (value != -1) {
                 out.write(value);
                 value = raw.read();
             }
-        }catch (IOException e) {
+        } catch (IOException e) {
 
         }
         return out.toString();
